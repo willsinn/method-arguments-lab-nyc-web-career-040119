e5d2cd1d4162a2de407924328420cef6a9e01a0a
w.h.sinn <w.h.sinn@gmail.com>
2019-03-19 14:42:35 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
 def 
   introduction_with_language(name, program)
-    puts "Hi, my name is #{name} and I am learning to program in #{program}"
+    puts "Hi, my name is #{name} and I am learning to program in #{program}."
 end
